6168c5059d429a3a3d3cf0a1b0202069db42733c
Eurika Kaiser <eurika.kaiser@gmail.com>
2020-08-07 14:26:03 -0700

    ongoing: linear system example with dmd

--- pykoopman/observables/__init__.py
+++ pykoopman/observables/__init__.py
@@ -1,4 +1,5 @@
 from ._identity import Identity
 from ._polynomial import Polynomial
 
-__all__ = ["Identity", "Polynomial"]
+__all__ = ["Identity",
+           "Polynomial"]
---
+++ pykoopman/observables/_observables.py
@@ -0,0 +1,71 @@
+"""
+Base class for specific observable classes
+"""
+import abc
+import numpy as np
+from sklearn.base import TransformerMixin
+from sklearn.utils.validation import check_is_fitted
+
+class BaseObservables(TransformerMixin):
+    """
+    Base class for observable classes.
+
+    Forces subclasses to implement 'fit', 'transform', and 'get_feature_names' functions
+    """
+
+    def __init__(self, **kwargs):
+        pass
+
+    @abc.abstractmethod
+    def fit(self, X, y=None):
+        """
+        Does not do anything for now.
+
+        Parameters
+        ----------
+        X : np.ndarray with shape [n_samples, n_features]
+
+        Returns
+        -------
+        self : instance
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def transform(self, X):
+        """
+        Transforms data.
+
+        Parameters
+        ----------
+        X : np.ndarray with shape [n_samples, n_features]
+            Data X is transformed row-wise.
+
+        Returns
+        -------
+        XT : np.ndarray with shape [n_samples, n_output_features]
+             Transformed data X into observables XT, where the number of
+             observables or features is n_output_features.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_feature_names(self, input_features=None):
+        """
+        Return names of observables.
+
+        Parameters
+        ----------
+        input_features : list of string of length n_features, optional
+            Default list is "x0", "x1", ..., "xn", where n = n_features.
+
+        Returns
+        -------
+        output_feature_names : list of string of length n_output_features
+        """
+        raise NotImplementedError
+
+    @property
+    def size(self):
+        check_is_fitted(self)
+        return self_n_output_features_
