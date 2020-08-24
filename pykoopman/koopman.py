a1c031edef4a2340cfddb2a9372626bf6a44f7ac
briandesilva <bdesilva@uw.edu>
2020-08-24 11:11:39 -0700

    Add docstrings

--- pykoopman/koopman.py
+++ pykoopman/koopman.py
@@ -12,7 +12,36 @@ from .regression import DMDRegressor
 
 
 class Koopman(BaseEstimator):
-    """Primary Discrete-Time Koopman class."""
+    """
+    Discrete-Time Koopman class.
+
+    Parameters
+    ----------
+    observables: observables object, optional (default ``Identity``)
+        Map(s) to apply to raw measurement data before estimating the
+        Koopman operator.
+        Must extend the ``pykoopman.observables.BaseObservables`` class.
+        The default option, ``Identity`` leaves the input untouched.
+
+    regressor: regressor object, optional (default ``DMD``)
+        The regressor used to learn the Koopman operator from the observables.
+        ``regressor`` can either extend the
+        ``pykoopman.regression.BaseRegressor`` class, or the ``pydmd.DMDBase``
+        class. In the latter case, the pydmd object must have both a ``fit``
+        and a ``predict`` method.
+
+    Attributes
+    ----------
+    model: sklearn.pipeline.Pipeline
+        Internal representation of the forward model.
+        Applies the observables and the regressor.
+
+    n_input_features_: int
+        Number of input features before computing observables.
+
+    n_output_features_: int
+        Number of output features after computing observables.
+    """
 
     def __init__(self, observables=None, regressor=None):
         if observables is None:
@@ -28,6 +57,20 @@ class Koopman(BaseEstimator):
         self.regressor = regressor
 
     def fit(self, x):
+        """
+        Fit the Koopman model by learning an approximate Koopman operator.
+
+        Parameters
+        ----------
+        x: numpy.ndarray, shape (n_examples, n_features)
+            Measurement data to be fit. Each row should correspond to an example
+            and each column a feature. It is assumed that examples are
+            equi-spaced in time (i.e. a uniform timestep is assumed).
+
+        Returns
+        -------
+        self: returns a ``Koopman`` instance
+        """
         x = validate_input(x)
 
         steps = [
@@ -44,32 +87,73 @@ class Koopman(BaseEstimator):
         return self
 
     def predict(self, x):
+        """
+        Predict the state one timestep in the future.
+
+        Parameters
+        ----------
+        x: numpy.ndarray, shape (n_samples, n_input_features)
+            Current state.
+
+        Returns
+        -------
+        y: numpy.ndarray, shape (n_samples, n_input_features)
+            Predicted state one timestep in the future.
+        """
         check_is_fitted(self, "model")
         return self.observables.inverse(self._step(x))
 
     def simulate(self, x0, n_steps=1):
+        """
+        Simulate an initial state forward in time with the learned Koopman
+        model.
+
+        Parameters
+        ----------
+        x0: numpy.ndarray, shape (n_input_features,)
+            Initial state from which to simulate.
+
+        n_steps: int, optional (default 1)
+            Number of forward steps to be simulated.
+
+        Returns
+        -------
+        y: numpy.ndarray, shape (n_steps, n_input_features)
+            Simulated states.
+            Note that ``y[0, :]`` is one timestep ahead of ``x0``.
+        """
         check_is_fitted(self, "model")
         # Could have an option to only return the end state and not all
         # intermediate states to save memory.
-        output = empty(
-            (n_steps, self.n_input_features_), dtype=self.koopman_matrix.dtype
-        )
-        output[0] = self.predict(x0)
+        y = empty((n_steps, self.n_input_features_), dtype=self.koopman_matrix.dtype)
+        y[0] = self.predict(x0)
         for k in range(n_steps - 1):
-            output[k + 1] = self.predict(output[k])
+            y[k + 1] = self.predict(y[k])
 
-        return output
+        return y
 
     def _step(self, x):
-        # TODO: rename this
+        """
+        Map x one timestep forward in the space of observables.
+
+        Parameters
+        ----------
+        x: numpy.ndarray, shape (n_examples, n_input_features)
+            State vectors to be stepped forward.
+
+        Returns
+        -------
+        X': numpy.ndarray, shape (n_examples, self.n_output_features_)
+            Observables one timestep after x.
+        """
         check_is_fitted(self, "model")
         return self.model.predict(x)
 
     @property
     def koopman_matrix(self):
         """
-        Get the Koopman matrix K such that
-        g(X') = g(X) * K
+        The Koopman matrix K satisfying g(X') = g(X) * K
+        where g denotes the observables map and X' denotes x advanced one timestep.
         """
         check_is_fitted(self, "model")
         return self.model.steps[-1][1].coef_
