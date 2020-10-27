9c89b5bbeadeeadf543fc0adcdf5e7ea88acd275
Eurika Kaiser <eurika.kaiser@gmail.com>
2020-10-26 18:22:02 -0700

    feat: added functions used for examples to common

--- pykoopman/common/__init__.py
+++ pykoopman/common/__init__.py
@@ -1,9 +1,12 @@
 from .validation import check_array
 from .validation import drop_nan_rows
 from .validation import validate_input
+from .examples import drss, advance_linear_system
 
 __all__ = [
     "check_array",
     "drop_nan_rows",
     "validate_input",
+    "drss"
+    "advance_linear_system"
 ]
