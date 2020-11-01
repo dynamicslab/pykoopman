8f3d0f5e633bb94c1cddc97fa2887d17ece63ba3
Eurika Kaiser <eurika.kaiser@gmail.com>
2020-11-01 10:57:31 -0800

    refactor: torus with control separate example in common

--- pykoopman/common/__init__.py
+++ pykoopman/common/__init__.py
@@ -1,8 +1,9 @@
 from .validation import validate_input
-from .examples import drss, advance_linear_system
+from .examples import drss, advance_linear_system, torus_with_control
 
 __all__ = [
     "validate_input",
     "drss"
     "advance_linear_system"
+    "torus_with_control"
 ]
