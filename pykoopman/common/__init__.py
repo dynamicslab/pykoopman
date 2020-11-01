f87881636bac2c4c30b6a7702a2b62a287418839
Eurika Kaiser <eurika.kaiser@gmail.com>
2020-11-01 11:53:00 -0800

    refactor: setup torus system

--- pykoopman/common/__init__.py
+++ pykoopman/common/__init__.py
@@ -1,9 +1,9 @@
 from .validation import validate_input
-from .examples import drss, advance_linear_system, torus_with_control
+from .examples import drss, advance_linear_system, setup_torus_dynamics
 
 __all__ = [
     "validate_input",
     "drss"
     "advance_linear_system"
-    "torus_with_control"
+    "setup_torus_dynamics"
 ]
