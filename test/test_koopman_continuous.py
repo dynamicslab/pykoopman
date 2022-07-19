445f7af94655f312449ea212bb3f1484c0ed1cb5
shaowu <shawnpan@umich.edu>
2022-07-18 21:30:15 -0400

    fixed bug in test_koopman_continuous.py

--- test/test_koopman_continuous.py
+++ test/test_koopman_continuous.py
@@ -1,4 +1,5 @@
 import pytest
+from pydmd import DMD
 from sklearn.utils.validation import check_is_fitted
 
 from pykoopman import KoopmanContinuous
@@ -13,7 +14,8 @@ def test_derivative_integration(data):
     x = data
 
     diff = Derivative(kind="finite_difference", k=1)
-    model = KoopmanContinuous(differentiator=diff)
+    dmd = DMD(svd_rank=2)
+    model = KoopmanContinuous(differentiator=diff, regressor=dmd)
 
     model.fit(x)
     check_is_fitted(model)
