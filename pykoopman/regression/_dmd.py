587edc0cb6ecb6d6fb322f83585b843e648900e4
shaowu <shawnpan@umich.edu>
2022-07-27 12:37:52 -0400

    add a few

--- pykoopman/regression/_dmd.py
+++ pykoopman/regression/_dmd.py
@@ -38,8 +38,19 @@ class DMDRegressor(BaseRegressor):
         self.n_samples_, self.n_input_features_ = x.shape
         # We transpose x because PyDMD assumes examples are columns, not rows
         self.regressor.fit(x.T)
+
+        # self.debug_coef_ = self.regressor.operator
+
+        # here we use a trick to get the `A' matrix, not the low rank one
+        # note that pydmd only stores the low-rank `A' matrix, which makes
+        # sense in high-dimensional system but we focus on low-dimensional,
+        # but highly nonlinear system
         self.coef_ = self.regressor.predict(identity(x.shape[1])).T
 
+        # Get Koopman modes, eigenvectors, eigenvalues manually at here
+        # self.modes_ = self.regressor.modes
+        self._amplitudes_ = self.regressor.amplitudes
+        self._eigenvalues_ = self.regressor.eigs
         return self
 
     def predict(self, x):
@@ -57,3 +68,13 @@ class DMDRegressor(BaseRegressor):
         """
         check_is_fitted(self, "coef_")
         return self.regressor.predict(x.T).T
+
+    @property
+    def eigenvalues_(self):
+        check_is_fitted(self, "coef_")
+        return self._eigenvalues_
+
+    @property
+    def amplitudes_(self):
+        check_is_fitted(self, "coef_")
+        return self._amplitudes_
