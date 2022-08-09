3108ab76c6956c27bb41e916bda64511d74bb194
Eurika Kaiser <eurika.kaiser@gmail.com>
2022-08-09 12:03:29 +0200

    feat: makes edmdc callable

--- pykoopman/observables/__init__.py
+++ pykoopman/observables/__init__.py
@@ -2,5 +2,6 @@ from ._custom_observables import CustomObservables
 from ._identity import Identity
 from ._polynomial import Polynomial
 from ._time_delay import TimeDelay
+from ._radial_basis_functions import RadialBasisFunction
 
-__all__ = ["CustomObservables", "Identity", "Polynomial", "TimeDelay"]
+__all__ = ["CustomObservables", "Identity", "Polynomial", "TimeDelay", "RadialBasisFunction"]
--- pykoopman/regression/__init__.py
+++ pykoopman/regression/__init__.py
@@ -1,5 +1,6 @@
 from ._base import BaseRegressor
 from ._dmd import DMDRegressor
 from ._dmdc import DMDc
+from ._edmdc import EDMDc
 
-__all__ = ["DMDRegressor", "DMDc"]
+__all__ = ["DMDRegressor", "DMDc", "EDMDc"]
